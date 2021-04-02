52e8fb8588f9ad006daca8fd8ed737ce91ce9825
Eric Markson <eric.markson@perficient.com>
2021-04-02 11:00:00 -0400

    Removing broken code

--- Scripts/UploadEpiPackage.ps1
+++ Scripts/UploadEpiPackage.ps1
@@ -51,12 +51,7 @@ Write-Host "Validation passed. Starting Deployment"
 
 #If the Module for Az.Storage is not found, install it using the force switch
 if (-not (Get-Module -Name Az.Storage -ListAvailable)) {
-
-    function Uninstall-AzureModules { $Modules = (Get-Module -ListAvailable AzureR*).Name |Get-Unique Foreach -parallel ($Module in $Modules) { Uninstall-Module $Module -Force } }
-
     Write-Host "Installing Az.Storage Powershell Module"
-    Uninstall-AzureModules 
-    Uninstall-AzureModules
     Install-Module -Name Az.Storage -Scope CurrentUser -Repository PSGallery -Force -AllowClobber
 }
 
