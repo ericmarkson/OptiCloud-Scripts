e1a385df21e7ce53b3e7b6f387ff3112559dd1df
Eric Markson <eric.markson@perficient.com>
2021-04-02 11:53:24 -0400

    Implement Azure.Storage instead of Az.Storage

--- Scripts/UploadEpiPackage.ps1
+++ Scripts/UploadEpiPackage.ps1
@@ -50,9 +50,9 @@ if([string]::IsNullOrWhiteSpace($ArtifactPath)){
 Write-Host "Validation passed. Starting Deployment"
 
 #If the Module for Az.Storage is not found, install it using the force switch
-if (-not (Get-Module -Name Az.Storage -ListAvailable)) {
+if (-not (Get-Module -Name Azure.Storage -ListAvailable)) {
     Write-Host "Installing Az.Storage Powershell Module"
-    Install-Module -Name Az.Storage -Scope CurrentUser -Repository PSGallery -Force -AllowClobber
+    Install-Module -Name Azure.Storage -Scope CurrentUser -Repository PSGallery -Force -AllowClobber
 }
 
 #Getting the latest Azure Storage Module
