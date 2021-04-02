748a6787ce60d62c94d2c4a4ea07a3e97eb66ec2
Eric Markson <eric.markson@perficient.com>
2021-04-02 11:56:11 -0400

    Change Logic

--- Scripts/UploadEpiPackage.ps1
+++ Scripts/UploadEpiPackage.ps1
@@ -50,10 +50,8 @@ if([string]::IsNullOrWhiteSpace($ArtifactPath)){
 Write-Host "Validation passed. Starting Deployment"
 
 #If the Module for Az.Storage is not found, install it using the force switch
-if (-not (Get-Module -Name Azure.Storage -ListAvailable)) {
     Write-Host "Installing Az.Storage Powershell Module"
     Install-Module -Name Azure.Storage -Scope CurrentUser -Repository PSGallery -Force -AllowClobber
-}
 
 #Getting the latest Azure Storage Module
 $env:PSModulePath = "C:\Modules\azurerm_6.7.0;" + $env:PSModulePath
