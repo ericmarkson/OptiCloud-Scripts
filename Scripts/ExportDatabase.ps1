db2faab52817cff1411506818aa3ddc5a94a2b76
Eric Markson <eric.markson@perficient.com>
2021-07-18 20:46:16 -0400

    Updating Export Database script to have the ability to download after export

--- Scripts/ExportDatabase.ps1
+++ Scripts/ExportDatabase.ps1
@@ -3,11 +3,12 @@
 #
 # Summary:     This script will take the a database (CMS or Commerce) for the
 #              specified environment and begin the export, and return a
-#              download link for the bacpac file.
+#              download link for the bacpac file. This will also download
+#              the database file based on the input params.
 #
-# Version:     1.1 - Added Fix for Azure.Storage Error
+# Version:     1.2 - Ability to download database to local location after export & custom name
 #
-# Last Updated: 4/2/2021
+# Last Updated: 7/17/2021
 #
 # Author: Eric Markson - eric.markson@perficient.com | eric@ericmarkson.com | https://optimizelyvisuals.dev/
 #
@@ -36,9 +37,13 @@ param
     [ValidateSet('epicms','epicommerce')]
     [String] $DatabaseName,
     [Parameter(Position=5)]
+    [String] $DownloadLocation,
+    [Parameter(Position=6)]
+    [String] $DownloadFileName,
+    [Parameter(Position=7)]
     [ValidateSet($true, $false, 0, 1)]
     [bool] $Wait = 0,
-    [Parameter(Position=6)]
+    [Parameter(Position=8)]
     [ValidateSet($true, $false, 0, 1)]
     [bool] $VerboseLogging = 0
     
@@ -54,6 +59,27 @@ if([string]::IsNullOrWhiteSpace($ClientSecret)){
 if([string]::IsNullOrWhiteSpace($ProjectID)){
     throw "A Project ID GUID is needed. Please supply one."
 }
+
+#Checking for Non-Interactive Shell
+function Test-IsNonInteractiveShell {
+    if ([Environment]::UserInteractive) {
+        foreach ($arg in [Environment]::GetCommandLineArgs()) {
+            #Test each Arg for match of abbreviated '-NonInteractive' command.
+            if ($arg -like '-NonI*') {
+                return $true
+            }
+        }
+    }
+
+    return $false
+}
+
+$IsInTheCloud = Test-IsNonInteractiveShell
+
+if($IsInTheCloud -eq  $true -and -not [string]::IsNullOrWhiteSpace($DownloadLocation)){
+    Write-Warning "Non-Interactive and/or Cloud shell detected. Functions may be limited for this script based on the parameters passed in."
+}
+
 
 Write-Host "Installing Azure.Storage Powershell Module"
 Install-Module -Name Azure.Storage -Scope CurrentUser -Repository PSGallery -Force -AllowClobber
@@ -129,15 +155,72 @@ if($status -eq "Failed"){
     throw "Export Failed."
 }
 
-        }
+    }
     else{
     $currExport = $export
     }
 
 $downloadLink = $currExport | Select -ExpandProperty "downloadLink"
 
-Write-Host "Export Finished. Download URL is: $downloadLink `n"
+Write-Host "`nExport Finished. Download URL is: $downloadLink `n"
 
 #Set the Output variable for the Export URL, if needed
-Write-Host "##vso[task.setvariable variable=ExportDownload;]'$downloadLink'"
+Write-Output "##vso[task.setvariable variable=ExportDownload;]'$downloadLink'" | Out-Null
 Write-Host "Output Variable Created. `nName: ExportDownload | Value: $downloadLink"
+
+if(-not [string]::IsNullOrWhiteSpace($DownloadLocation)){
+    
+    Write-Host "`n`nStarting the download process..."
+
+    $downloadLink -match "^(https://)?([^.]+)\.blob\.core([^?]*)/([^?]*)?([^.]*)" | Out-Null
+
+    $fileName = "{0}_{1}" -f $Matches[2],$Matches[4]
+    if(-not [string]::IsNullOrWhiteSpace($DownloadFileName)){
+        if($DownloadFileName -like "*.bacpac"){
+            $fileName = $DownloadFileName
+            } 
+        else{
+            $fileName = "{0}{1}" -f $DownloadFileName, ".bacpac"
+            }
+    }
+
+    $fileContext = $Matches[2]
+
+    #Function to download Database
+    Function DownloadDatabaseFile 
+    {  
+   
+        #check if folder exists
+        $destination=$DownloadLocation+"\"+$fileContext
+        $folderExists=Test-Path -Path $destination 
+
+        if($folderExists -eq $false)  
+        {  
+            Write-Host "`nDownload Location does not exist. Creating it now!"  
+            #Create the new folder  
+            $newStructure = New-Item -ItemType Directory -Path $destination
+            Write-Host "Folder Structure Created At $($newStructure.FullName)"
+        }     
+    
+        $elapsedTime = [system.diagnostics.stopwatch]::StartNew()
+        $startTime = Get-Date
+        Write-Host "`nDownload Starting...Started at: $startTime"
+
+        Invoke-WebRequest $downloadLink -OutFile $("{0}\{1}" -f $destination,$fileName)
+
+        $elapsedTime.stop()
+
+        Write-Host "Download Completed Successfully!`n`nDownload Location: $destination`nFile Name: $fileName`nStarted at: $startTime`nCompleted at: $(Get-Date)`nTime to Download: $([string]::Format("{0:d2}:{1:d2}:{2:d2}", $elapsedTime.Elapsed.hours, $elapsedTime.Elapsed.minutes, $elapsedTime.Elapsed.seconds))"
+
+        #Set the Output variable for the Download Location, if needed
+        Write-Output "##vso[task.setvariable variable=DownloadLocation;]'$destination'" | Out-Null
+        Write-Host "`nOutput Variable Created. `nName: DownloadLocation | Value: $destination"
+
+        #Set the Output variable for the Saved Filename, if needed
+        Write-Output "##vso[task.setvariable variable=FileName;]'$fileName'" | Out-Null
+        Write-Host "`nOutput Variable Created. `nName: FileName | Value: $fileName"
+    }   
+  
+    DownloadDatabaseFile
+
+}
